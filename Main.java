15252b18832eeb15dddda1e2c706680085a468de
Ronit <ronitk8484@gmail.com>
2026-09-20 21:15:32 +0530

    Solved HashSet

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import collections.ArrayListClass;
+import collections.HashSetClass;
 import collections.LinkedListClass;
 
 void main() {
@@ -6,4 +7,6 @@ void main() {
     arrayListClass.solutions();
     LinkedListClass linkedListClass = new LinkedListClass();
     linkedListClass.solutions();
+    HashSetClass hashSetClass = new HashSetClass();
+    hashSetClass.solutions();
 }
---
+++ collections/HashSetClass.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.*;
+
+public class HashSetClass {
+    public void solutions(){
+        System.out.println("1. Remove duplicates from an array using HashSet.");
+//
+//        2. Check if two arrays have common elements.
+//        3. Find union of two arrays.
+//        4. Find intersection of two arrays.
+//        5. Find first duplicate element.
+//        6. Check if array contains duplicate values.
+//        7. Count unique words in a sentence.
+//        8. Find missing numbers from 1 to N.
+//        9. Remove duplicate characters from a string.
+//        10. Find distinct vowels in a string.
+    }
+}
