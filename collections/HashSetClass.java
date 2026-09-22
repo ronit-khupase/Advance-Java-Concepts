7d36f0101e3d5fa0132101cb2356a4f124452683
Ronit <ronitk8484@gmail.com>
2026-09-22 20:23:14 +0530

    Solved HashSet

--- collections/HashSetClass.java
+++ collections/HashSetClass.java
@@ -15,13 +15,73 @@ public class HashSetClass {
         }
         System.out.println("Array After Removing Duplicates : "+set);
 
-//
-//        2. Check if two arrays have common elements.
-//        3. Find union of two arrays.
-//        4. Find intersection of two arrays.
-//        5. Find first duplicate element.
-//        6. Check if array contains duplicate values.
-//        7. Count unique words in a sentence.
+        System.out.println("2. Check if two arrays have common elements.");
+        int[] a = {15,25,10,30,20,10,40};
+        set = new HashSet<>();
+        for(int num : arr){
+            set.add(num);
+        }
+        boolean common = false;
+        for (int num : a){
+            if(set.remove(num)){
+                common = true;
+                break;
+            }
+        }
+        if(common){
+            System.out.println("Common Elements Present!");
+        }else {
+            System.out.println("No Common Elements Present!");
+        }
+
+        System.out.println("3. Find union of two arrays.");
+        set = new HashSet<>();
+        for (int num : arr){
+            set.add(num);
+        }
+        for (int num : a){
+            set.add(num);
+        }
+        System.out.println("Union of 2 arrays : "+set);
+
+
+        System.out.println("4. Find intersection of two arrays.");
+        set = new HashSet<>();
+        for (int num : arr){
+            set.add(num);
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int num : a){
+            if(set.contains(num)){
+                list.add(num);
+            }
+        }
+        System.out.println("Intersection of Arrays: "+list);
+
+
+        System.out.println("5. Find first duplicate element.");
+        set = new HashSet<>();
+        for (int num : arr){
+            if(!set.add(num)){
+                System.out.println("First Duplicate is : "+num);
+                break;
+            }
+        }
+
+        System.out.println("6. Check if array contains duplicate values.");
+        set = new HashSet<>();
+        for (int num : arr){
+            set.add(num);
+        }
+        if(arr.length != set.size()){
+            System.out.println("Array Contains Duplicate Elements!");
+        }else {
+            System.out.println("Array Not Contains Duplicates!");
+        }
+
+        System.out.println("7. Count unique words in a sentence.");
+
+
 //        8. Find missing numbers from 1 to N.
 //        9. Remove duplicate characters from a string.
 //        10. Find distinct vowels in a string.
