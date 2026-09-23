4026384ff98c01b9548f92fd176ca01c688b879d
Ronit <ronitk8484@gmail.com>
2026-09-23 22:37:57 +0530

    Solved HashSet

--- collections/HashSetClass.java
+++ collections/HashSetClass.java
@@ -23,7 +23,7 @@ public class HashSetClass {
         }
         boolean common = false;
         for (int num : a){
-            if(set.remove(num)){
+            if(set.contains(num)){
                 common = true;
                 break;
             }
@@ -50,13 +50,14 @@ public class HashSetClass {
         for (int num : arr){
             set.add(num);
         }
-        List<Integer> list = new ArrayList<>();
-        for(int num : a){
-            if(set.contains(num)){
-                list.add(num);
+        HashSet<Integer> intersection = new HashSet<>();
+
+        for (int num : a) {
+            if (set.contains(num)) {
+                intersection.add(num);
             }
         }
-        System.out.println("Intersection of Arrays: "+list);
+        System.out.println("Intersection of Arrays: "+intersection);
 
 
         System.out.println("5. Find first duplicate element.");
@@ -80,10 +81,38 @@ public class HashSetClass {
         }
 
         System.out.println("7. Count unique words in a sentence.");
+        String str = sc.nextLine().toLowerCase();
+        String[] s = str.split("\\s+");
+        HashSet<String> strSet = new HashSet<>(Arrays.asList(s));
+        System.out.println("Unique words in a String are : "+strSet.size());
 
+        System.out.println("8. Find missing numbers from 1 to N.");
+        Integer[] arr1 = new Integer[]{1, 2, 3, 4, 4, 6, 7, 9, 1, 12, 13};
+        set = new HashSet<>(Arrays.asList(arr1));
+        int n = 15;
+        System.out.print("Not Present Elements are : ");
+        for(int i = 1; i <= n; i++){
+            if(!set.contains(i)){
+                System.out.print(i+" ");
+            }
+        }
 
-//        8. Find missing numbers from 1 to N.
-//        9. Remove duplicate characters from a string.
-//        10. Find distinct vowels in a string.
+        System.out.println("9. Remove duplicate characters from a string.");
+        str = sc.nextLine().toLowerCase();
+        HashSet<Character> charSet = new HashSet<>();
+        char[] ch = str.toCharArray();
+        for(char c : ch){
+            charSet.add(c);
+        }
+        System.out.println("String After Removing Repeated Characters : "+charSet);
+
+        System.out.println("10. Find distinct vowels in a string.");
+        charSet = new HashSet<>();
+        for (char c : ch){
+            if(c == 'a' ||c == 'e' ||c == 'i' ||c == 'o' ||c == 'u'){
+                charSet.add(c);
+            }
+        }
+        System.out.println("Different Vowels in the String are : "+charSet);
     }
 }
